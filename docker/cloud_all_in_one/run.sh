6472bd394532efec9543d4f0aa3b69a39cf74942
girotte-tao <taoware@qq.com>
2023-12-26 11:42:57 +0800

    feat delete out-of-date images after compose

--- docker/cloud_all_in_one/run.sh
+++ docker/cloud_all_in_one/run.sh
@@ -1,1 +1,3 @@
-docker-compose up --build
+docker-compose build --no-cache # rebuild images with the latest code and config
+docker-compose up # compose all the docker images up
+docker rmi $(docker images -f "dangling=true" -q) # delete out-of-date images
